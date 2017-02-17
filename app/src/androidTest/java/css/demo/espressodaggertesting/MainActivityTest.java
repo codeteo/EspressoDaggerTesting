b29eecee7f229c79c5602e07e7c89472e7b5356e
teo <ramones.teo@gmail.com>
2017-02-17 20:25:57 +0200

    Injects mock dependency to SUT

--- app/src/androidTest/java/css/demo/espressodaggertesting/MainActivityTest.java
+++ app/src/androidTest/java/css/demo/espressodaggertesting/MainActivityTest.java
@@ -1,8 +1,24 @@
 package css.demo.espressodaggertesting;
 
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+import javax.inject.Inject;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by css on 2/17/17.
@@ -10,6 +26,34 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
+    @Inject
+    Clock clock;
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(
+            MainActivity.class,
+            true,     // initialTouchMode
+            false);   // launchActivity. False so we can customize the intent per test method
+
+
+    @Before
+    public void setUp() {
+        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+        MyApplication app
+                = (MyApplication) instrumentation.getTargetContext().getApplicationContext();
+        MockDemoComponent component = (MockDemoComponent) app.component();
+        component.inject(this);
+    }
+
+    @Test
+    public void today() {
+        Mockito.when(clock.getNow()).thenReturn(new DateTime(2001, 9, 21, 0, 0, 0));
+
+        activityRule.launchActivity(new Intent());
+
+        onView(withId(R.id.date))
+                .check(matches(withText("2001-09-21")));
+    }
 
 
 }
--- app/src/main/java/css/demo/espressodaggertesting/MainActivity.java
+++ app/src/main/java/css/demo/espressodaggertesting/MainActivity.java
@@ -1,14 +1,20 @@
 package css.demo.espressodaggertesting;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
+
+import org.joda.time.DateTime;
 
 import javax.inject.Inject;
+
+import static css.demo.espressodaggertesting.DateUtils.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MAIN-ACTIVITY";
+    public static final String KEY_MILLIS = "millis";
 
     @Inject
     Clock clock;
@@ -17,10 +23,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView todayView = (TextView) findViewById(R.id.date);
 
         ((MyApplication) getApplication()).component().inject(this);
 
         Log.i(TAG, "onCreate clock: " + clock.getNow());
+
+
+        long millis = getIntent().getLongExtra(KEY_MILLIS, -1);
+        DateTime dateTime = (millis > 0) ? new DateTime(millis) : clock.getNow();
+        todayView.setText(format(dateTime));
 
     }
 }
